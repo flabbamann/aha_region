50ec5de305db5b605f2a28c4e0c83903f3256f55
Lars <flabbamann@users.noreply.github.com>
2023-10-18 20:36:51 +0200

    Define CONFIG_SCHEMA
    this fixes a hassfest validation warning

--- custom_components/aha_region/__init__.py
+++ custom_components/aha_region/__init__.py
@@ -1,6 +1,11 @@
 """aha custom component."""
 
 from homeassistant import core
+import homeassistant.helpers.config_validation as cv
+
+from .const import DOMAIN
+
+CONFIG_SCHEMA = cv.platform_only_config_schema(DOMAIN)
 
 
 async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
